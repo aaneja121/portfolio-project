5c7c0e5e35140ca73f61d0e97db1520a34ec17d4
aaneja121 <aaneja06@gmail.com>
2025-03-04 12:04:41 -0500

    Update LineupComponentEnhanced.java

--- src/LineupComponentEnhanced.java
+++ src/LineupComponentEnhanced.java
@@ -1,3 +1,20 @@
-public interface LineupComponentEnhanced {
+/**
+ * Enhanced interface for LineupComponent.
+ * This component will further enhance the team's lineup with methods not provided in the basic interface.
+ */
+public interface LineupComponentEnhanced extends LineupComponentKernel {
 
+    /**
+     * Generates and shows the best lineup based on points per game.
+     *
+     * @ensures the highest-scoring player for each position is in the starting lineup
+     */
+    void makeBest();
+
+    /**
+     * Prints out the current lineup without the update.
+     *
+     * @ensures every player's essentials are displayed (name, position, PPG)
+     */
+    void displayLineup();
 }
